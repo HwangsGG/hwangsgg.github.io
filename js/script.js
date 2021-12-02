fef7b594cb99d52db86d8779f2a517243e9a7524
Hwangs <hwangsgg@gmail.com>
2021-12-02 19:38:17 +0200

    validate form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Slider
 $(document).ready(function(){
   $('.carousel__inner').slick({
     speed: 1200,
@@ -10,18 +11,22 @@ $(document).ready(function(){
         {
           breakpoint: 768,
           settings: {
+          autoWidth: true,
           // dots: true,
           arrows: false,
           }
         }
       ]
   });
+
+//Tabs
   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });   
-  
+
+//Catalog
   function toggleSlide(item) {
     $(item).each(function(i) {
         $(this).on('click', function(e) {
@@ -34,4 +39,46 @@ $(document).ready(function(){
 
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item_back');
+
+//Modal
+$('[data-modal=consultation]').on('click', function() {
+  $('.overlay, #consultation').fadeIn('slow');
+});
+$('.modal__close').on('click', function() {
+  $('.overlay, #consultation, #thanks , #order').fadeOut('slow');
+});
+
+$('.button_catalog').each(function(i) {
+  $(this).on('click', function() {
+    $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+    $('.overlay, #order').fadeIn('slow');
+  })
+})
+//Validate forms
+function valideForms (form) {
+  $(form).validate({
+    rules: {
+      name: "required",
+      phone: "required",
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      name: "Пожалуйста введите своё имя",
+      phone: "Пожалуйста введите номер телефона",
+      email: {
+        required: "Пожалуйста введите свой E-mail",
+        email: "E-mail адрес указан неверно"
+      }
+    }
+  });
+}
+valideForms('#consultation form');
+valideForms('#consultation-form');
+valideForms('#order form');
+
+//Phone mask
+$('input[name=phone]').mask("+38 (099) 999-99-99");
 });
